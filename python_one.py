87a85bfa06a3d82a94dbf7c6c7ff2153cd117023
Godwin <ekainugodwin@gmail.com>
2020-01-28 11:41:22 +0100

    second assignment

--- python_one.py
+++ python_one.py
@@ -24,3 +24,28 @@ def upper_and_lower(word):
     return num_of_upper, num_of_lower
 
 upper_and_lower('God is Good')
+
+
+# Write a Python function that takes a number as a parameter and 
+# check the number is prime or not.
+# Note : A prime number (or a prime) is a natural 
+# number greater than 1 and that has no positive 
+# divisors other than 1 and itself.
+
+
+def isprime(x):
+    if x > 1:
+    # check for factors
+        for i in range(2,x):
+            if (x % i) == 0:
+                print("is not a prime number")
+                break
+        else:
+            print("is a prime number")
+        
+    # if input number is less than
+    # or equal to 1, it is not prime
+    else:
+        print("is not a prime number")
+
+isprime(17)
